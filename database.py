9f10ac87339afa6e1425a2c0cd88a6f9aac10955
Abhishekkannur <106137218+Abhishekkannur@users.noreply.github.com>
2023-07-26 16:09:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL='postgresql://postgres:556677@localhost:5432/employee_db'
 
-engine= create_engine(SQLALCHEMY_DATABASE_URL,pool_pre_ping=True)
+engine= create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal=sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
